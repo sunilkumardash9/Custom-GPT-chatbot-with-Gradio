970f00f547fbef346247daf97dbc5d9a726c284c
Sunil Kumar Dash <47926185+sunilkumardash9@users.noreply.github.com>
2023-05-03 18:10:13 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,24 +1,51 @@
 import gradio as gr
-import openai, os
+import openai
+import os 
+import json
+import time
 from dotenv import load_dotenv
 
+
 load_dotenv()
-key = os.environ.get('OPENAI_API_KEY')
+key = os.environ.get('KEY')
 openai.api_key = key
 
-messages = []
+messages  = []
 cost = 0
+images = []
 
+def save_history(history):
+    with open("saved_history.json", "w") as f:
+        json.dump(history, f)
+
+def load_history():
+    try:
+        with open("saved_history.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    
 def add_text(history, text): 
     global messages
     history = history + [(text,'')]
     messages = messages + [{"role":'user', 'content': text}]
-    #save_history(history)
+    save_history(history)
     return history, ""
+
+def add_file(history, file):
+    history = history + [((file.name,), None)]
+    return history
+
+def add_text_for_images(history, text):
+    history = history + [(text,'')]
+
+    return history
+
 
 def generate_response(history, ):
         global messages, cost
     
+  
         response = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             messages=messages,
@@ -35,43 +62,84 @@ def generate_response(history, ):
             #time.sleep(0.05)
             yield history
 
-
-def add_text_for_images(history, text):
-    history = history + [(text,'')]
-    return history
+        save_history(history)
+        
+    
 
 def generate_image(history, text):
-    #img_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-T6WvejtuZhCqGecac5Yz862J/user-8LOO4C5fnrZYzYKwpILUEVUF/img-W3B70rebpSI4Gqqb2PJpPlsP.png?st=2023-04-24T02%3A11%3A04Z&se=2023-04-24T04%3A11%3A04Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-24T03%3A04%3A50Z&ske=2023-04-25T03%3A04%3A50Z&sks=b&skv=2021-08-06&sig=yn/05MJTXiYSf5ECQmDgZKkoo5fvTu2jlDbcKzeFSbk%3D"
-    img_url = '/home/sunil/Downloads/profilepic.jpg'
+
+    response = openai.Image.create(
+        prompt = text,
+        n=1,
+        size = "256x256"
+
+    )
+    img_url = response['data'][0]['url']
     history[-1][1] = (img_url,'')
+    print(history)
     return history
+   
+    # except:
+    #     return [("Error",'')]
+    
+
+def bot(history):
+    response = "**That's cool!**"
+    history[-1][1] = response
+    return history
+    
 
 def calc_cost():
     global cost
+
     return round(cost,3)
+
+history_ = load_history()
+print('yes')
+
 
 with gr.Blocks() as demo:
     
-    radio = gr.Radio(value='gpt-3.5-turbo', choices=['gpt-3.5-turbo','gpt-4'], label='models')
-    chatbot = gr.Chatbot(value=[], elem_id="chatbot").style(height=650)
-    with gr.Row():
-        with gr.Column(scale=0.90):
-            txt = gr.Textbox(
-                show_label=False,
-                placeholder="Enter text and press enter, or upload an image",
-            ).style(container=False) 
-        with gr.Column(scale=0.10):
-            cost_view = gr.Textbox(label='usage in $',value=0)
+    with gr.Tab(label='ChatGPT'):
+        # with gr.Column(scale=0.05):
+        #radio = gr.Radio(['gpt-3.5-turbo', 'gpt-4'], value = 'gpt-3.5-turbo', label = 'Choose Model', interactive=True).style(container=True)
 
-    txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
+        chatbot = gr.Chatbot(value=history_, elem_id="chatbot").style(height=650)
+        
+        with gr.Row():   
+            with gr.Column(scale=0.85):
+                txt = gr.Textbox(
+                    show_label=False,
+                    placeholder="Enter text and press enter, or upload an image",
+                ).style(container=False)
+            with gr.Column(scale=0.10, min_width=0):
+                #btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+               # btn = gr.Button('click', )
+                cost_view = gr.Textbox(label='usage in $',value=cost)
+        
+        txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
             generate_response, inputs =[chatbot,],outputs = chatbot,).then(calc_cost, outputs=cost_view)
+        
+    with gr.Tab(label='Dalle-2'):
+            chatbot = gr.Chatbot(value=[], elem_id="Dalle-2").style(height=750)
+            with gr.Row():
+                with gr.Column(scale=0.70):
+                    txt = gr.Textbox(
+                        show_label=False,
+                        placeholder="Enter text and press enter, or upload an image",
+                    ).style(container=False)
+                with gr.Column(scale=0.15, min_width=0):
+                    btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+                with gr.Column(scale=0.15):
+                    cost_view = gr.Textbox(label='usage in $',value=cost)
+        
+            txt.submit(add_text_for_images, [chatbot, txt], chatbot).then(
+                generate_image, [chatbot,txt], chatbot ).then(calc_cost, outputs=cost_view)
             
+            btn.upload(add_file, [chatbot, btn], [chatbot]).then(
+        bot, chatbot, chatbot)
+  
 
-    # txt.submit(add_text_for_images, [chatbot, txt], chatbot).then(
-    #             generate_image, [chatbot,txt], chatbot )
 demo.queue()
-
-
 if __name__ == "__main__":
-    
     demo.launch()  
